66f0122ca6b91596955d3322130d5a2e29be306a
Edmar <edmar.bevi@gmail.com>
2025-05-15 12:45:35 -0300

    chore: add onclick event to the arrowdown

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -46,9 +46,11 @@ const HeroSection = () => {
       </div>
       
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <a href="#about" className="text-foreground/60 hover:text-foreground transition-colors">
-          <ArrowDown className="w-6 h-6" />
-        </a>
+          <a href=""
+            onClick={scrollToSection('about')}
+          >
+            <ArrowDown className="w-6 h-6" />
+          </a>
       </div>
     </section>
   );
